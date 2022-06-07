275f29388ae31d91852c693df9539971ee71ad10
yamachu <yamachu.dev@gmail.com>
2022-06-08 03:34:44 +0900

    参照が変わったらhandlerを解除しておく

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import React from "react";
 import { f0_data, phoneme_data } from "./data.js";
 
 console.log(self.crossOriginIsolated);
+const worker = new Worker("./static/js/worker.js");
 
 function App() {
-  const worker = React.useMemo(() => new Worker("./static/js/worker.js"));
   React.useEffect(() => {
-    worker.addEventListener("message", (e) => {
+    const messageHandler = (e) => {
       console.log(e);
-    });
-    worker.addEventListener("error", (e) => {
+    };
+    const errorHandler = (e) => {
       console.error(e);
-    });
+    };
+    worker.addEventListener("message", messageHandler);
+    worker.addEventListener("error", errorHandler);
+
+    return () => {
+      worker.removeEventListener("message", messageHandler);
+      worker.removeEventListener("error", errorHandler);
+    };
   }, [worker]);
   const inference = async () => {
     worker.postMessage({ f0_data, phoneme_data });
