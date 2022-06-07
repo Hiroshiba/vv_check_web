ff3b75fdac35e2953b378a2d287c2d2bed812e93
yamachu <yamachu.dev@gmail.com>
2022-06-08 03:38:35 +0900

    add reference

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,3 +1,4 @@
+// see: https://github.com/facebook/create-react-app/issues/10210#issuecomment-873286336
 module.exports = function (app) {
   app.use(function (req, res, next) {
     res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
